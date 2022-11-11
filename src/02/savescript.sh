46eac50ab7eb9bc614d1d83d6991cc74a8b5d422
Alvin Thalia <alvintha@in-f2.nsk.21-school.ru>
2022-11-11 14:42:19 +0700

    first commit

--- src/02/savescript.sh
+++ src/02/savescript.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-if [ $# -ne 4 ]
-  then
-    echo "Error. Need to enter 4 parameter, you have entered $# parameters!"
-else
-  for var in "$@"
-  do
-  if [ $(bash isnumber.sh $var) -eq 0 ]
-    then
-      echo "Error. Need to enter a numeric parameter, you entered a text parameter!"
-    exit
-  elif [ $var -gt 6 ] || [ $var -lt 1 ]
-    then
-      echo "Error. The parameters must be in the range from 1 to 6."
-    exit
-  fi
-  done
-  if [ $1 -eq $2 ] || [ $3 -eq $4 ]
-    then
-      echo "Error. The background color and font color must be different (the first and second parameters and the third and fourth parameters must not match)."
-      echo "Please try again!"
-    exit
-  fi
-  sh systemresearch.sh $1 $2 $3 $4
-fi
+read -r -p "Want to write data to a file? [Y/N]:" input
+case $input in
+  [Yy]* ) cp $1 $(date +"%d_%m_%y_%H_%M_%S").status
+esac
